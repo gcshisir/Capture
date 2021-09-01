3f08372ba3dab943325a5643ccbe03d0c13c4545
Shisir GC <shisir.gc44@gmail.com>
2021-09-01 21:53:01 +0545

    solved footer overflow issue

--- react projects/react-web-app/src/components/Footer.js
+++ react projects/react-web-app/src/components/Footer.js
@@ -3,7 +3,7 @@ import React, { Fragment } from "react";
 const Footer = () => {
   return (
     <Fragment>
-      <div className="footer d-flex justify-content-center align-items-center">
+      <div className="footer d-flex justify-content-center align-items-center py-2 overflow-hidden">
         <h3>Copyright &copy; 2020</h3>
       </div>
     </Fragment>
